97e57132773823c8b33275aa790a75021a49e112
LeBuddies <gsock9299@naver.com>
2026-04-15 10:19:29 +0900

    feat: Swagger OpenAPI 설정 (JWT 인증 포함) - closes #6

---
+++ src/main/java/com/lebuddies/budget/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.lebuddies.budget.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "bearerAuth";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("💰 개인 가계부 REST API")
+                        .description("Spring Boot + PostgreSQL + JWT 기반 개인 가계부 백엔드 API 서버")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
