4f2754c636b3f204232f16bd2ade0bc8bd59070e
LeBuddies <gsock9299@naver.com>
2026-04-15 10:10:43 +0900

    feat: 메인 애플리케이션 클래스 추가

---
+++ src/main/java/com/lebuddies/budget/BudgetApplication.java
@@ -0,0 +1,12 @@
+package com.lebuddies.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApplication.class, args);
+	}
+}
